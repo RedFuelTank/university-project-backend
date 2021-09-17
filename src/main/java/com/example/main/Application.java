af879035652b66b0543cdafe3d1b4da051d4eca1
kitimo <kitimo@ttu.ee>
2021-09-17 15:45:04 +0300

    controllet method

--- src/main/java/com/example/main/Application.java
+++ src/main/java/com/example/main/Application.java
@@ -2,12 +2,23 @@ package com.example.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+
+    @GetMapping
+    public String hello() {
+        return "Hello, World";
+    }
 }
