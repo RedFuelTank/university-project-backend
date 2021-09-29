fa6d645958096199fddf5b15cec18496548533fd
kitimo <kitimo@ttu.ee>
2021-09-29 22:03:29 +0300

    upd user

--- src/main/java/com/example/main/model/User.java
+++ src/main/java/com/example/main/model/User.java
@@ -8,83 +8,84 @@ import java.util.Optional;
 
 @Entity
 public class User {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String username;
-  private String password;
-  private String email;
-  private String name;
-  private String surname;
-  private String phoneNumber;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private String surname;
+    private String phoneNumber;
 
 
-  public User(String username, String password, String email, String name, String surname, String phoneNumber) {
-    this.username = username;
-    this.password = password;
-    this.email = email;
-    this.name = name;
-    this.surname = surname;
-    this.phoneNumber = phoneNumber;
-  }
+    public User(String username, String password, String email, String name, String surname, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
 
-  public User() {
+    public User() {
 
-  }
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public Optional<String> getEmail() {
-    return Optional.ofNullable(this.email);
-  }
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(this.email);
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getSurname() {
-    return surname;
-  }
+    public String getSurname() {
+        return surname;
+    }
 
-  public Optional<String> getPhoneNumber() {
-    return Optional.ofNullable(this.phoneNumber);
-  }
+    public Optional<String> getPhoneNumber() {
+        return Optional.ofNullable(this.phoneNumber);
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setSurname(String surname) {
-    this.surname = surname;
-  }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
-  public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
+
