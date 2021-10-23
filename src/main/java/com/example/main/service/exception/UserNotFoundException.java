a4fae396b67c9dd96055f89bb5c6384b44330db3
Kirill Timofejev <kitimo@ttu.ee>
2021-10-23 13:35:13 +0300

    Error changes

--- src/main/java/com/example/main/service/exception/UserNotFoundException.java
+++ src/main/java/com/example/main/service/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package com.example.main.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, code = HttpStatus.NOT_FOUND, reason = "User not found")
 public class UserNotFoundException extends RuntimeException {
 }
