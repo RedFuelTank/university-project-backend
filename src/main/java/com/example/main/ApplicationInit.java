ed2585a8145d4df3204b89a6de510931eff95990
Mikhail Kozhevnikov <mikohz@ttu.ee>
2021-11-27 11:01:59 +0200

    testing for error

--- src/main/java/com/example/main/ApplicationInit.java
+++ src/main/java/com/example/main/ApplicationInit.java
@@ -21,27 +21,27 @@ public class ApplicationInit implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-//      List<Advertisement> advertisements = List.of(
-//              new Advertisement("Walk with my dog", "Walk with my dog for 30 euro pls", 1, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("I am able to walk with your dog", "I can walk with your dog for 15 euro", 1, 0, 0, "", "25-10-2022", Advertisement.Type.OFFER),
-//              new Advertisement("Ремонт на кухне", "Срочно требуется какой-нибудь строитель, который может сдлеать ремонт на кухне. В частности поставить новую плиту и микроволновку, Цена за работу 50 евро", 1, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Тиммейты дорогие делайте работу", "БЫСТРО СДЕЛАЛИ СВОИ ФРОНТЕНДЫ И БЭКЕНДЫ ПОРТКИ СВОИ ПОДОБРАЛИ, Я ВАМ ПЛАТИТЬ НЕ БУДУ, МЫ РАБОТАЕМ ЗА ИДЕЮ", 2, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-4 - AUTO DEPLOY WORKS", "blank", 2, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-5", "blank", 2, 0, 0, "", "10-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-6", "blank", 2, 0, 0, "", "15-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-7", "blank", 2, 0, 0, "", "28-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-8", "blank", 2, 0, 0, "", "06-10-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-9 TEST", "blank", 2, 0, 0, "", "25-01-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-10", "blank", 2, 0, 0, "", "25-03-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-11", "blank", 2, 0, 0, "", "25-07-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-12", "blank", 2, 0, 0, "", "25-11-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-13", "blank", 2, 0, 0, "", "25-08-2022", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-14", "blank", 2, 0, 0, "", "25-10-2023", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-15", "blank", 2, 0, 0, "", "25-10-2026", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-16", "blank", 2, 0, 0, "", "25-10-2025", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-17", "blank", 2, 0, 0, "", "25-10-2030", Advertisement.Type.REQUEST),
-//              new Advertisement("Request-18", "blank", 2, 0, 0, "", "25-10-2027", Advertisement.Type.REQUEST));
-//      advertisementRepository.saveAll(advertisements);
+      List<Advertisement> advertisements = List.of(
+              new Advertisement("Walk with my dog", "Walk with my dog for 30 euro pls", 1, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("I am able to walk with your dog", "I can walk with your dog for 15 euro", 1, 0, 0, "", "25-10-2022", Advertisement.Type.OFFER),
+              new Advertisement("Ремонт на кухне", "Срочно требуется какой-нибудь строитель, который может сдлеать ремонт на кухне. В частности поставить новую плиту и микроволновку, Цена за работу 50 евро", 1, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Тиммейты дорогие делайте работу", "БЫСТРО СДЕЛАЛИ СВОИ ФРОНТЕНДЫ И БЭКЕНДЫ ПОРТКИ СВОИ ПОДОБРАЛИ, Я ВАМ ПЛАТИТЬ НЕ БУДУ, МЫ РАБОТАЕМ ЗА ИДЕЮ", 2, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-4 - AUTO DEPLOY WORKS", "blank", 2, 0, 0, "", "25-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-5", "blank", 2, 0, 0, "", "10-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-6", "blank", 2, 0, 0, "", "15-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-7", "blank", 2, 0, 0, "", "28-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-8", "blank", 2, 0, 0, "", "06-10-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-9 TEST", "blank", 2, 0, 0, "", "25-01-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-10", "blank", 2, 0, 0, "", "25-03-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-11", "blank", 2, 0, 0, "", "25-07-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-12", "blank", 2, 0, 0, "", "25-11-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-13", "blank", 2, 0, 0, "", "25-08-2022", Advertisement.Type.REQUEST),
+              new Advertisement("Request-14", "blank", 2, 0, 0, "", "25-10-2023", Advertisement.Type.REQUEST),
+              new Advertisement("Request-15", "blank", 2, 0, 0, "", "25-10-2026", Advertisement.Type.REQUEST),
+              new Advertisement("Request-16", "blank", 2, 0, 0, "", "25-10-2025", Advertisement.Type.REQUEST),
+              new Advertisement("Request-17", "blank", 2, 0, 0, "", "25-10-2030", Advertisement.Type.REQUEST),
+              new Advertisement("Request-18", "blank", 2, 0, 0, "", "25-10-2027", Advertisement.Type.REQUEST));
+      advertisementRepository.saveAll(advertisements);
 
       List<User> users = List.of(new User("RedFuelTank", "12345", "danila.romanof@gmail.com",
                       "Danila", "Romanov", "55503565"),
