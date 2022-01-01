b43f343a93f9f8f0022be73bf1719022a83f1eae
Pavel Grubeljas <pagrub@ttu.ee>
2022-01-01 21:44:51 +0200

    SkateboardsApi.java remaster

--- src/main/java/com/example/main/controller/theory/skateboards/SkateboardsApi.java
+++ src/main/java/com/example/main/controller/theory/skateboards/SkateboardsApi.java
@@ -1,5 +1,14 @@
 package com.example.main.controller.theory.skateboards;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RequestMapping("/skateboards")
+@RestController
 public class SkateboardsApi {
 
     //todo The Story
@@ -18,22 +27,85 @@ public class SkateboardsApi {
     // - button to update existing skateboard
     // - button to delete skateboard
 
+    private final List<Skateboard> skateboards = new ArrayList<>();
+
     //todo A first things first, please add necessary annotations to this class
 
     //todo B "an overview of the skateboards we sell"
     // create a method to query skateboards (plural)
+    @GetMapping()
+    public List<Skateboard> getSkateboards(@RequestParam(required = false) Optional<String> inStock,
+                               @RequestParam(required = false) Optional<String> condition,
+                               @RequestParam(required = false) Optional<String> sort,
+                               @RequestParam(required = false) Optional<String> reverse) {
+
+        List<Skateboard> skateboardsCopy = skateboards;
+
+        if (inStock.isPresent()) {
+            skateboardsCopy = skateboardsCopy.stream().filter(s -> s.getInStock().equals(inStock))
+                    .collect(Collectors.toList());
+        }
+        if (condition.isPresent()) {
+            skateboardsCopy = skateboardsCopy.stream().filter(s -> s.getCondition().equals(condition))
+                    .collect(Collectors.toList());
+        }
+        if (sort.isPresent()) {
+            if (sort.equals("name"))
+                skateboardsCopy.sort(Comparator.comparing(skateboard -> skateboard.getName()));
+            else if (sort.equals("price")) {
+                skateboardsCopy.sort(Comparator.comparing(skateboard -> skateboard.getPrice()));
+            }
+        }
+        if (reverse.isPresent()) Collections.reverse(skateboardsCopy);
+
+        return skateboardsCopy;
+    }
+
 
     //todo C "page for each skateboard where I can see it's info"
     // create a method to query a single skateboard
 
+    @GetMapping("{id}")
+    public Skateboard getSkateboard(@PathVariable int id) {
+        return skateboards.stream().filter(c -> c.getId() == id).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
     //todo D "button to add a new skateboard"
     // create a method to save a new skateboard
+
+    @PostMapping()
+    public void saveSkateboard(@RequestBody Skateboard skateboard) {
+        skateboards.add(skateboard);
+    }
 
     //todo E "button to update existing skateboard"
     // create a method to update a skateboard
 
+    @PatchMapping("{id}")
+    public Skateboard updateSkateboard(@PathVariable int id, @RequestBody Skateboard updateSkateboard) {
+        Skateboard skateboard = getSkateboard(id);
+
+        return update(skateboard, updateSkateboard);
+    }
+
+    public Skateboard update(@PathVariable Skateboard skateboard, @RequestBody Skateboard updateSkateboard) {
+        skateboard.setName(updateSkateboard.getName());
+        skateboard.setCondition(updateSkateboard.getCondition());
+        skateboard.setDesigner(updateSkateboard.getDesigner());
+        skateboard.setPrice(updateSkateboard.getPrice());
+        skateboard.setInStock(updateSkateboard.getInStock());
+        return skateboard;
+    }
+
     //todo F "button to delete skateboard"
     // create a method to delete a skateboard
+
+    @DeleteMapping("{id}")
+    public void deleteSkateboard(@PathVariable int id) {
+        Skateboard skateboard = getSkateboard(id);
+        skateboards.remove(skateboard);
+    }
 
     //todo G, H "I want to know which ones are in stock and which ones are new (vs used)"
     // G modify correct method to filter whether the skateboard is in stock or out of stock
